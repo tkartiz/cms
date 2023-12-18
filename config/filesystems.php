33e9964394a2a6ca1bdae8c995cbfdebaf98c90b
tkartiz <t.kamiya@artiz-creative.co.jp>
2023-12-18 15:05:22 +0900

    57th commit:LP ver 7.4 honban file systems permission changed

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,6 +33,14 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'permissions' => [ // permissions 設定値を追記
+                'dir' => [
+                    'public'  => 0775, // public なディレクトリは775で作成 
+                ],
+                'file' => [
+                    'public' => 0664, // public なファイルは664で作成
+                ],
+            ]
         ],
 
         'public' => [

5c1d96dcf45d7b5660373382f07c70ca8b37f2fe
tkartiz <t.kamiya@artiz-creative.co.jp>
2023-12-28 09:13:28 +0900

    90th commit:LP ver10.8 honban noindex tag delete in head.php

--- public/preview/asset/include/head.php
+++ public/preview/asset/include/head.php
@@ -15,7 +15,6 @@ if ($level === 3) {
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="description" content="湘南初の道の駅。潮風薫る“ちがさき愛”いっぱいの交流拠点として、令和7年7月（予定）にオープンいたします！ゆったりとした雰囲気によるリラクゼーションを提供する休憩機能、さまざまなニーズに対応した情報提供と魅力・資源を発信する情報発信機能、地域とのつながり“ちがさき愛”を育み発信する地域連携機能を兼ね備えた道の駅です。">
     <meta name="keywords" content="道の駅, 湘南ちがさき, ALOHA湘南, 潮風, ちがさき愛, リラクゼーション, 休憩, 情報発信, 地域連携, 柳島, しおさい公園, キャンプ場, 国道134号, ファーマーズフォレスト, 直売所, 野菜, スイーツ, 観光, グランドオープン">
-    <meta name="robots" content="noindex">
 
     <title>道の駅　湘南ちがさき</title>
 
--- resources/views/m-shonanchigasaki.blade.php
+++ resources/views/m-shonanchigasaki.blade.php
@@ -2,9 +2,33 @@
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>「道の駅 湘南ちがさき」ページ遷移</title>
+    <!-- LPと同じ内容を記載 -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="湘南初の道の駅。潮風薫る“ちがさき愛”いっぱいの交流拠点として、令和7年7月（予定）にオープンいたします！ゆったりとした雰囲気によるリラクゼーションを提供する休憩機能、さまざまなニーズに対応した情報提供と魅力・資源を発信する情報発信機能、地域とのつながり“ちがさき愛”を育み発信する地域連携機能を兼ね備えた道の駅です。">
+    <meta name="keywords" content="道の駅, 湘南ちがさき, ALOHA湘南, 潮風, ちがさき愛, リラクゼーション, 休憩, 情報発信, 地域連携, 柳島, しおさい公園, キャンプ場, 国道134号, ファーマーズフォレスト, 直売所, 野菜, スイーツ, 観光, グランドオープン">
+
+    <meta property="og:locale" content="ja">
+    <meta property="og:url" content="https://m-shonanchigasaki.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="湘南初の道の駅。潮風薫る「ちがさき愛」いっぱいの交流拠点として、令和7年7月（予定）にオープンいたします！" />
+    <meta property="og:description" content="ゆったりとした雰囲気によるリラクゼーションを提供する休憩機能、さまざまなニーズに対応した情報提供と魅力・資源を発信する情報発信機能、地域とのつながり“ちがさき愛”を育み発信する地域連携機能を兼ね備えた道の駅です。" />
+    <meta property="og:site_name" content="道の駅 湘南ちがさき" />
+    <meta property="og:image" content="https://m-shonanchigasaki.com/m-shonanchigasaki.com.png" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:description" content="ゆったりとした雰囲気によるリラクゼーションを提供する休憩機能、さまざまなニーズに対応した情報提供と魅力・資源を発信する情報発信機能、地域とのつながり“ちがさき愛”を育み発信する地域連携機能を兼ね備えた道の駅です。" />
+
+    <meta name="auther" content="茅ヶ崎市">
+    <meta name="description" content="ゆったりとした雰囲気によるリラクゼーションを提供する休憩機能、さまざまなニーズに対応した情報提供と魅力・資源を発信する情報発信機能、地域とのつながり“ちがさき愛”を育み発信する地域連携機能を兼ね備えた道の駅です。">
+    <meta name="keywords" content="道の駅, 湘南ちがさき, ALOHA湘南, 潮風, ちがさき愛, リラクゼーション, 休憩, 情報発信, 地域連携, 柳島, しおさい公園, キャンプ場, 国道134号, ファーマーズフォレスト, 直売所, 野菜, スイーツ, 観光, グランドオープン">
+
+    <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="expires" content="0">
+
+    <title>道の駅　湘南ちがさき</title>
+    <!-- LPと同じ内容を記載 -->
 </head>
 
 <body>
